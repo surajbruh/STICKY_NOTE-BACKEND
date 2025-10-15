b09ee16c44f20b25607274d13f06cb720ae4dc66
suraj <imsurajyadav044@gmail.com>
2025-10-15 16:00:00 +0530

    created /api/note/notes API endpoint route handler

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,7 +4,7 @@ import Note from "../models/noteModel.js"
 const router = express.Router()
 export default router
 
-router.post("/api/note/upload", async (req, res) => {
+router.post("/api/notes/upload", async (req, res) => {
     try {
         const { content } = req.body
         if (!content) return res.status(400).json({ message: "Content is  required" })
@@ -16,9 +16,18 @@ router.post("/api/note/upload", async (req, res) => {
             Note: newNote
         })
     } catch (error) {
-        console.error("/API/UPLOAD ENDPOING ERROR", error.message)
+        console.error("/API/UPLOAD ENDPOINT ERROR", error.message)
         return res.status(500).json("Something went wrong")
     }
 })
 
 // TODO: ADD GET, DELETE ROUTE
+router.get("/api/note/notes", async (req, res) => {
+    try {
+        const notes = await Note.find()
+        res.status(200).json(notes)
+    } catch (error) {
+        console.error("/API/NOTES ENDPOINT ERROR", error.message)
+        return res.status(500).json("Something went wrong")
+    }
+})
